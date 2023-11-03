d3403a7f79c9e656eb59bd822ac3fc51ff332592
erasmorojastech <erasmorojastech@gmail.com>
2023-11-03 12:00:00 -0300

    Add MainContent component full styled

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import '../assets/styles/MainContent.css'
+
+const MainContent = function () {
+    return(
+      <main>
+        <h2>Fun facts about React</h2>
+        <ul>
+          <li>Nothing is funny here</li>
+          <li>Nothing is funny here</li>
+          <li>Nothing is funny here</li>
+          <li>Nothing is funny here</li>
+          <li>Nothing is funny here</li>
+        </ul>
+      </main>
+    );
+  }
+  
+  export default MainContent;
