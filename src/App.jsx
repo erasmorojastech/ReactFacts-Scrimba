998954eb3eeeaf29d795e030afd631ee050212f1
erasmorojastech <erasmorojastech@gmail.com>
2023-11-04 12:00:00 -0300

    Add MainComponent to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import MainContent  from './components/MainContent'
 
 function App() {
 
   return (
     <>
       <Header />
+      <MainContent />
     </>
   )
 }
